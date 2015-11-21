fe4b4ad8f4aa8d38401d9b1332003d9d62626f46
gordondalos <gordondalos@gmail.com>
2015-11-21 14:30:41 +0600

    поправил замечания

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+
+	<!-- Chatra {literal} -->
+
+	ChatraID = '7TQGG5RqXJjTSYaJW';
+	(function(d, w, c) {
+		var n = d.getElementsByTagName('script')[0],
+			s = d.createElement('script');
+		w[c] = w[c] || function() {
+				(w[c].q = w[c].q || []).push(arguments);
+			};
+		s.async = true;
+		s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+			+ '//chat.chatra.io/chatra.js';
+		n.parentNode.insertBefore(s, n);
+	})(document, window, 'Chatra');
+
+		<!-- /Chatra {/literal} -->
+
 // это попап
 	$(function() {
 		$( "#dialog" ).dialog({
